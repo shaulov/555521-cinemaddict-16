c69135a2552acd2f7a7deca7bf5448193a426777
shaulov <shineginn@gmail.com>
2021-12-13 18:09:47 +0300

    Unite view and mock data

--- src/view/card-view.js
+++ src/view/card-view.js
@@ -1,16 +1,21 @@
-export const createCardTemplate = () => (`
+import {COMMENTS} from '../mock/film.js';
+
+export const createCardTemplate = ({name, poster, rating, realiseDate, duration, description, genres, commentId}) => {
+  const visibleDescription = description.length > 140 ? `${description.slice(0, 139)}...` : description;
+
+  return `
   <article class="film-card">
     <a class="film-card__link">
-      <h3 class="film-card__title">The Dance of Life</h3>
-      <p class="film-card__rating">8.3</p>
+      <h3 class="film-card__title">${name}</h3>
+      <p class="film-card__rating">${rating}</p>
       <p class="film-card__info">
-        <span class="film-card__year">1929</span>
-        <span class="film-card__duration">1h 55m</span>
-        <span class="film-card__genre">Musical</span>
+        <span class="film-card__year">${realiseDate.slice(-4)}</span>
+        <span class="film-card__duration">${duration}</span>
+        <span class="film-card__genre">${genres[0]}</span>
       </p>
-      <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-      <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-      <span class="film-card__comments">5 comments</span>
+      <img src="./images/posters/${poster}" alt="" class="film-card__poster">
+      <p class="film-card__description">${visibleDescription}</p>
+      <span class="film-card__comments">${COMMENTS[commentId].length} comments</span>
     </a>
     <div class="film-card__controls">
       <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
@@ -18,4 +23,5 @@ export const createCardTemplate = () => (`
       <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
     </div>
   </article>
-`);
+  `;
+};
