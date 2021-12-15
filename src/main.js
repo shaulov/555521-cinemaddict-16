3ce18bca3eef42bf055a612d2d250e24dc0e3c24
shaulov <shineginn@gmail.com>
2021-12-15 19:17:12 +0300

    Do refactoring film data

--- src/main.js
+++ src/main.js
@@ -10,9 +10,13 @@ import {createDetailGenre} from './view/detail-genre-view.js';
 import {createFilmCommentContainer} from './view/comment-container-view.js';
 import {createFilmComment} from './view/comment-view.js';
 import {createNewCommentContainer} from './view/new-comment-view.js';
-import {filmsData, COMMENTS} from './mock/film.js';
+import {generateFilm, COMMENTS} from './mock/film.js';
+import {generateFilter} from './mock/filter.js';
 
 const FILM_COUNT = 5;
+
+const films = Array.from({length: 15}, generateFilm);
+const filters = generateFilter(films);
 
 const RenderPosition = {
   BEFOREBEGIN: 'beforebegin',
@@ -29,7 +33,7 @@ const siteHeader = document.querySelector('.header');
 renderTemplate(siteHeader, createUserRateTemplate(), RenderPosition.BEFOREEND);
 
 const siteMain = document.querySelector('.main');
-renderTemplate(siteMain, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMain, createSiteMenuTemplate(filters), RenderPosition.BEFOREEND);
 renderTemplate(siteMain, createSortTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(siteMain, createContentContainerTemplate(), RenderPosition.BEFOREEND);
 
@@ -41,13 +45,13 @@ renderTemplate(footerStatistic, createStatisticTemplate(), RenderPosition.BEFORE
 renderTemplate(filmListContainer, createShowMoreButtonTemplate(), RenderPosition.AFTEREND);
 
 for (let i = 0; i < FILM_COUNT; i++) {
-  renderTemplate(filmListContainer, createCardTemplate(filmsData[i]), RenderPosition.BEFOREEND);
-  renderTemplate(siteFooter, createInfoPopupTemplate(filmsData[1]), RenderPosition.AFTEREND);
+  renderTemplate(filmListContainer, createCardTemplate(films[i]), RenderPosition.BEFOREEND);
+  renderTemplate(siteFooter, createInfoPopupTemplate(films[0]), RenderPosition.AFTEREND);
 
   const filmDetailsContainer = document.querySelector('.film-details__table');
-  renderTemplate(filmDetailsContainer, createDetailGenre(filmsData[1].genres), RenderPosition.BEFOREEND);
+  renderTemplate(filmDetailsContainer, createDetailGenre(films[0].genres), RenderPosition.BEFOREEND);
 
-  const comments = COMMENTS[filmsData[i].commentId];
+  const comments = COMMENTS[films[i].comments];
 
   const filmCommentContainer = document.querySelector('.film-details__bottom-container');
   renderTemplate(filmCommentContainer, createFilmCommentContainer(comments.length), RenderPosition.BEFOREEND);
