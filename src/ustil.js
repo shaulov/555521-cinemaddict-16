b1028fcd53edae78d56760662c2b42f59514b558
shaulov <shineginn@gmail.com>
2021-12-15 19:19:37 +0300

    Create util module

---
+++ src/ustil.js
@@ -0,0 +1,13 @@
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+export const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+
+export const isWatchlist = () => {
+
+};
