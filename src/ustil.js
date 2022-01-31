2ae5e1cfe6f064477fdfaf7baa37efe4ac8887a1
shaulov <shineginn@gmail.com>
2022-01-31 16:20:40 +0300

    Add replace function

--- src/ustil.js
+++ src/ustil.js
@@ -9,3 +9,16 @@ export const getRandomInteger = (a = 0, b = 1) => {
 
 export const isEscapeKey = (evt) => evt.key === 'Escape';
 
+export const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
